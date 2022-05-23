a36812b8de692e652a2ccfc7c0ca64fa9bb7a8fb
Yemi-S <zpfmgy@hotmail.com>
2022-05-23 18:47:15 -0500

    Consumo de API Backend - Editar producto

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,6 +1,7 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { tap } from 'rxjs/operators';
 import { Producto } from 'src/app/models/producto';
@@ -13,19 +14,25 @@ import { ProductoService } from 'src/app/services/producto.service';
 })
 export class CrearProductoComponent implements OnInit {
   productoForm: FormGroup;
+  titulo: string = 'CREAR PRODUCTO';
+  id: string | null;
+
   constructor(private fb: FormBuilder,
               private router: Router, 
               private toastr: ToastrService,
-              private productoSvc: ProductoService) { 
+              private productoSvc: ProductoService,
+              private aRouter: ActivatedRoute) { 
     this.productoForm = this.fb.group({
       producto: ['', Validators.required],
       categoria: ['', Validators.required],
       ubicacion: ['', Validators.required],
       precio: ['', Validators.required],
     });
+    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
+    this.esEditar();
   }
 
   agregarProducto(){
@@ -37,11 +44,34 @@ export class CrearProductoComponent implements OnInit {
       precio: this.productoForm.get('precio')?.value,
     }
 
-    this.productoSvc.guardarProducto(productoData).pipe(
-      tap(()=>this.toastr.success('El producto fue registrado con éxito!','Producto Registrado')),
-      tap(()=>this.router.navigate(['/'])),
-    ).subscribe({
-      error : (error)=>console.log(error)  
-    });
+    if (this.id !== null){
+      this.productoSvc.editarProducto(this.id, productoData).pipe(
+        tap(()=>this.toastr.info('El producto fue actualizado con éxito!','Producto Actualizado')),
+        tap(()=>this.router.navigate(['/'])),
+      ).subscribe({
+        error : (error)=>console.log(error)  
+      });
+    } else {
+      this.productoSvc.guardarProducto(productoData).pipe(
+        tap(()=>this.toastr.success('El producto fue registrado con éxito!','Producto Registrado')),
+        tap(()=>this.router.navigate(['/'])),
+      ).subscribe({
+        error : (error)=>console.log(error)  
+      });
+    }
+  }
+
+  esEditar(): void{
+    if (this.id !== null){
+      this.titulo = "EDITAR PRODUCTO";
+      this.productoSvc.obtenerProducto(this.id).pipe(
+        tap((data)=> this.productoForm.setValue({
+          producto: data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio
+        }))
+      ).subscribe();
+    }
   }
 }
